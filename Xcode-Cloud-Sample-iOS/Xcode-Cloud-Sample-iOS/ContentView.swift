854984bf6a585db791ad74d535ddc906cc497c25
Qasim Abbas <qasimabbas52@gmail.com>
2022-11-13 10:54:21 -0500

    Initial Commit

---
+++ Xcode-Cloud-Sample-iOS/Xcode-Cloud-Sample-iOS/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Xcode-Cloud-Sample-iOS
+//
+//  Created by Qasim Abbas on 11/13/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, Xcode Cloud!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Xcode-Cloud-Sample-iOS/Xcode-Cloud-Sample-iOS/Xcode_Cloud_Sample_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Xcode_Cloud_Sample_iOSApp.swift
+//  Xcode-Cloud-Sample-iOS
+//
+//  Created by Qasim Abbas on 11/13/22.
+//
+
+import SwiftUI
+
+@main
+struct Xcode_Cloud_Sample_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
